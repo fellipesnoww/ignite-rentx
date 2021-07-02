c1254382c583c8bbf20590127e1e3b40276dc2e3
Fellipe <fellipe_emanoel@univem.edu.br>
2021-07-01 21:41:11 -0300

    Started creation Image Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -4,8 +4,10 @@ import { StatusBar } from 'react-native';
 import { 
     Container,
     Header,
+    CarImages
 } from './styles';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 export function CarDetails(){
   
@@ -14,6 +16,9 @@ export function CarDetails(){
         <Header>
           <BackButton onPress={() => {}}/>
         </Header>
+        <CarImages>
+          <ImageSlider imagesUrl={['https://img1.gratispng.com/20180331/izq/kisspng-audi-rs-6-audi-rs6-audi-a6-car-audi-5abf3cdc8a3545.0356772815224823965661.jpg']}/>
+        </CarImages>
       </Container>
   );
 } 
