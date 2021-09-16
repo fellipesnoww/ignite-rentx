43c75ab3a482131f8e822c7ffcfe37e4748ac0e0
Fellipe Neves <fellipe_emanoel@univem.edu.br>
2021-09-15 21:23:12 -0300

    Started scheduling screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+    Container,
+    Header,
+    Title,
+    RetalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer,
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function Scheduling(){
+    const theme = useTheme();
+    return (
+        <Container>
+            <Header>
+                <StatusBar
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton color={theme.colors.shape} onPress={() => {}}/>
+                <Title>
+                    Escolha uma{'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel
+                </Title>
+
+                <RetalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue selected={false}>
+                            16/10/2021
+                        </DateValue>
+                    </DateInfo>
+
+                    <ArrowSvg/>
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+                        <DateValue selected={false}>
+                            16/10/2021
+                        </DateValue>
+                    </DateInfo>
+                </RetalPeriod>
+            </Header>
+
+            <Content>
+
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar"/>
+            </Footer>
+        </Container>
+    )
+}
