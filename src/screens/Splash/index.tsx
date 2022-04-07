cce4673ba15219abcca307445538220ff9f24cf5
Fellipe Neves <fellipe_emanoel@univem.edu.br>
2022-04-06 22:32:27 -0300

    Created first animation with reanimated and started implemetation of splash screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, StyleSheet, Dimensions } from 'react-native';
+
+import Animated, 
+{ useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
+
+import { 
+    Container,
+} from './styles';
+
+const WIDTH = Dimensions.get('window').width;
+
+export function Splash() {
+
+  const animation = useSharedValue(0);
+  const animatedSyles = useAnimatedStyle(() => {
+    return {
+      transform: [{
+        translateX: withTiming(animation.value, {
+          duration: 3000,
+          easing: Easing.bezier(.73, .17, 0, 1.01)
+        })
+      }]
+    }
+  });
+
+  function handleAnimationPosition(){
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
+  return (
+      <Container>
+        <Animated.View style={[styles.box, animatedSyles]}/>
+
+        
+        <Button title="Mover" onPress={handleAnimationPosition}/>   
+      </Container>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+
+  }
+})
