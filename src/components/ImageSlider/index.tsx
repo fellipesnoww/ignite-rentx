17c9dc12fa136c99f8a77f0791d8914c6d86dc4e
Fellipe Neves <fellipe2.0e@gmail.com>
2022-09-26 23:30:20 -0300

    using new bullet component

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,11 +1,11 @@
 import React, { useRef } from 'react';
 import { useState } from 'react';
 import { FlatList, ViewToken } from 'react-native';
+import Bullet from '../Bullet';
 
 import { 
     Container,
-    ImageIndexes,
-    ImageIndex,
+    ImageIndexes,    
     CarImageWrapper,
     CarImage,
 } from './styles';
@@ -31,7 +31,7 @@ export function ImageSlider({ imagesUrl }: Props) {
       <Container>
             <ImageIndexes>
               {imagesUrl.map((_, index) => (
-                  <ImageIndex 
+                  <Bullet 
                     key={String(index)}
                     active={index === imageIndex}
                   />
