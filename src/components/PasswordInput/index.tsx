bf7d23ff5a19c77ae2b0083e84ed04cce9424770
Fellipe Neves <fellipe2.0e@gmail.com>
2022-09-26 21:44:43 -0300

    Created new input components

---
+++ src/components/PasswordInput/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { TextInputProps } from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+import { ChangePasswordVisibilityButton, Container, IconContainer, InputText } from './styles';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+interface Props extends TextInputProps{
+    iconName: React.ComponentProps<typeof Feather>['name']
+}
+
+export default function PasswordInput({iconName, ...rest}: Props){
+    const theme = useTheme();
+    const [isPasswordVisibile, setIsPasswordVisibile] = useState(true);
+
+    function handlePasswordVisibilityChange(){
+        setIsPasswordVisibile(!isPasswordVisibile);
+    }
+
+    return (
+        <Container>
+            <IconContainer>
+                <Feather 
+                    name={iconName}
+                    size={24}
+                    color={theme.colors.text_detail}
+                />
+            </IconContainer>
+            <InputText 
+                secureTextEntry={isPasswordVisibile}
+                {...rest}
+            />
+            <BorderlessButton onPress={handlePasswordVisibilityChange}>
+                <IconContainer>
+                    <Feather 
+                        name={isPasswordVisibile ? "eye" : "eye-off"}
+                        size={24}
+                        color={theme.colors.text_detail}
+                    />
+                </IconContainer>
+            </BorderlessButton>
+        </Container>
+    )
+}
