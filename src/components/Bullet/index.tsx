4038c3a4da2af1d154de0b978a0435470fa3a160
Fellipe Neves <fellipe2.0e@gmail.com>
2022-09-26 23:30:05 -0300

    Created a new component to bullet list

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./styles";
+
+interface Props {
+    active?: boolean;
+}
+
+
+export default function Bullet({active = false}: Props){
+    return (
+        <Container active={active}/>                    
+    )
+}
